e56c942a56c2b3a371f88992e8d8b19ad7449384
nikmorror37 <nikitamironchuk37@gmail.com>
2023-03-23 19:12:09 +0300

    Initial commit

---
+++ HomeTask16LoginPage/HomeTask16LoginPage/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using HomeTask16LoginPage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HomeTask16LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        string userNameExisted = "Leonardo";
+        string passwordExisted = "1234567890";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        public IActionResult LoginPageError()
+        {
+            return NotFound("Неверно(ы) Имя_пользователя или(и) Пароль");
+        }
+
+        [HttpPost]
+        public RedirectResult Login(string userName, string password)
+        {
+            if (userNameExisted.Equals(userName) && passwordExisted.Equals(password))
+            {
+                return Redirect("~/Home/Index");
+            }
+            else
+                return Redirect("~/Home/LoginPageError");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ HomeTask16LoginPage/HomeTask16LoginPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeTask16LoginPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
